e974a7fdf427b108a4a99aaec250fe1745013090
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-07-22 21:14:48 -0300

    page temp

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 
 export default function Page() {
   return (
       <div>
         <h1>Temp</h1>
+        <Link href="/">Home</Link>
+        <Link href="/primeiro">Primeiro</Link>
+
       </div>
   );
 }
