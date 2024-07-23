63c8c31c4b5fe914efb81966367f7b79dced27e4
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-07-22 21:15:33 -0300

    page app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import '../styles/reset.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-      <div>
+      <div className="border-2 border-[#04ee18]">
         <h1>Home</h1>
+        <nav className="flex justify-start gap-4 items-center">
+          <Link href="/temp">Temp</Link>
+          <Link href="/primeiro">Primeiro</Link>
+          <Link href="/flexbox">Flexbox</Link>
+        </nav>
       </div>
   );
 }
